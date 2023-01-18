be3c0fe6f28f92f5c7e2f5b61146c6d813388f96
김관우/글로벌개발팀 <sam.kim@musinsa.com>
2023-01-18 20:22:04 +0900

    123

--- app/src/main/java/review/collector/app/App.java
+++ app/src/main/java/review/collector/app/App.java
@@ -7,5 +7,7 @@ package review.collector.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        System.out.println("Hello World!!2");
+        System.out.println("Hello World!!3");
     }
 }
